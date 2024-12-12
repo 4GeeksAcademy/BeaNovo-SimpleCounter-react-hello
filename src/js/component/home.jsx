044a0b7b9c07b6048c3938cf380a782dd02553fc
BeaNovo <168178819+BeaNovo@users.noreply.github.com>
2024-12-12 09:52:23 +0000

    Fisrt Commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,38 @@
 import React from "react";
-
-//include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
+
+
+
+
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="row">
+				<div className="col">
+                {/* <FontAwesomeIcon icon={faClock} /> */}
+				{/* <i class="fa fa-clock text-white"></i> */}
+				<i className="fa-regular fa-clock"></i>
+				</div>
+				<div className="col">
+					{props.num6}
+				</div>
+				<div className="col">
+					{props.num5}
+				</div>
+				<div className="col">
+					{props.num4}
+				</div>
+				<div className="col">
+					{props.num3}
+				</div>
+				<div className="col">
+					{props.num2}
+				</div>
+				<div className="col">
+					{props.num1}
+				</div>
+			</div>
 		</div>
 	);
 };
